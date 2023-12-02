c3a0b9dca90cc31fa65356f8a9c32024ba0f5d8d
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2023-12-02 12:07:28 +0300

    new commit

--- src/main/java/com/example/movieAPI/Review.java
+++ src/main/java/com/example/movieAPI/Review.java
@@ -1,4 +1,20 @@
 package com.example.movieAPI;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+
+
 }
