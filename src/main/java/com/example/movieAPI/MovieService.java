23006bbd3a09e4d2cfca22d389e7e170239ae3c1
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2023-12-02 15:12:28 +0300

    new commit

---
+++ src/main/java/com/example/movieAPI/MovieService.java
@@ -0,0 +1,15 @@
+package com.example.movieAPI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> AllMovies(){
+        return movieRepository.findAll();
+    }
+}
